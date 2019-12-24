175f184dae5301b909685aa2ddb18541005f7bd8
romvtk <romvtk@mail.ru>
2019-12-24 23:25:34 +0300

    Готовый header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
+$('.header__slider').slick({
+    infinite: true,
+    fade:true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="left"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="left"></img>',
+    asNavFor: '.slider-dots'
+  
+});
 
+$('.slider-dots').slick({
+slidesToShow: 4,
+slidesToScroll: 4,
+asNavFor: '.header__slider'
+});
 
   
 });
