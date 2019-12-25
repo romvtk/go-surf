4bf2027f7e0e8cf4fcc3fcf7e6aab665acb66eb4
romvtk <romvtk@mail.ru>
2019-12-25 22:55:03 +0300

    готовый второй блок Map

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
-$(function(){
+$(function () {
 
-$('.header__slider').slick({
-    infinite: true,
-    fade:true,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="left"></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="left"></img>',
-    asNavFor: '.slider-dots'
-  
-});
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="left"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="left"></img>',
+        asNavFor: '.slider-dotshead'
 
-$('.slider-dots').slick({
-slidesToShow: 4,
-slidesToScroll: 4,
-asNavFor: '.header__slider'
-});
+    });
 
-  
+    $('.slider-dotshead').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="left"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="left"></img>',
+        asNavFor: '.slider-map',
+    })
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
+    })
+
 });
