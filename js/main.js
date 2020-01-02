d659a66d684e53a56d6dfb1c9b2a2284306da528
romvtk <romvtk@mail.ru>
2020-01-02 17:09:13 +0300

    готовая черновая версия

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(function () {
     });
 
 
-    $('.holder__slider').slick({
+    $('.holder__slider, .shop__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="left"></img>',
@@ -82,5 +82,9 @@ $(function () {
     var summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
 
     $('.summ').html('$' + summ);
+
+    $('.surfboard-box__circle').on('click', function(){
+        $(this).toggleClass('active');
+    })
 
 });
